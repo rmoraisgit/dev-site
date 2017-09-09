45fc112a828620e833ae8eab6243e64e6a0256d1
rsilveiragit <rafamorais.br@live.com>
2017-09-09 13:35:05 -0300

    Adicionado teste

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+var arrayPacientes = document.querySelectorAll(".paciente");
+
+arrayPacientes.forEach(function(paciente){
+    var peso = paciente.querySelector(".info-peso");
+    var altura = paciente.querySelector(".info-altura");
+    var imc = paciente.querySelector(".info-imc");
+    imc.textContent = peso.textContent / (altura.textContent*altura.textContent);
+})
+
+console.log("teste");
