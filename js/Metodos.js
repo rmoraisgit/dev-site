07b6e9e161f7c8d0b7f978f8a387005d34ca7807
rsilveiragit <rafamorais.br@live.com>
2017-09-10 15:38:57 -0300

    Adicionado limitação de 2 digitos para casa decimal após a virgula, no calculo do IMC

--- js/Metodos.js
+++ js/Metodos.js
@@ -1,14 +1,42 @@
 function CalculaImc(peso, altura){
     var imc = peso / (altura*altura);
-    return imc;
+    return imc.toFixed(2);
 }
 
 function CriaClassePaciente(form){
     var Paciente = {
-        nome: form.nome,
-        peso: form.peso,
-        altura: form.altura,
-        gordura: form.gordura,
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
         imc: CalculaImc(form.peso.value, form.altura.value)
     };
+    return Paciente;
+}
+
+
+function CriaTd(dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add = classe;
+    return td;
+}
+
+
+function CriaTr(ObjetoPaciente){
+    var trObjetoPaciente = document.createElement("tr");
+
+    var tdNome = CriaTd(ObjetoPaciente.nome, "info-nome");
+    var tdPeso = CriaTd(ObjetoPaciente.peso, "info-peso");
+    var tdAltura = CriaTd(ObjetoPaciente.altura, "info-altura");
+    var tdGordura = CriaTd(ObjetoPaciente.gordura, "info-gordura");
+    var tdImc = CriaTd(ObjetoPaciente.imc, "info-imc");
+
+    trObjetoPaciente.appendChild(tdNome);
+    trObjetoPaciente.appendChild(tdPeso);
+    trObjetoPaciente.appendChild(tdAltura);
+    trObjetoPaciente.appendChild(tdGordura);
+    trObjetoPaciente.appendChild(tdImc);
+
+    return trObjetoPaciente;
 }
